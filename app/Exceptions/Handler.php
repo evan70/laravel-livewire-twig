f4bd6bef2dc361c7b63adcb1ea7d0bfb97d2478d
evan70 <ivanpaldan@yandex.com>
2024-02-05 21:10:16 +0100

    add exception handler

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+}
